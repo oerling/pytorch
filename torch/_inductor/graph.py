e6432fb92a596ed6650f848a4dec58132a4c591f
Benjamin Glass <bglass@openteams.com>
2026-05-07 17:14:10 +0000

    [inductor][bisector] Fix decomposition/lowering debug info
    Fixes debug info (i.e. overload names) for decompositions and lowerings by maintaining the call counter -> debug info dictionary in a file.  Previously, the mapping was inaccessible in cases where the code under test was spawned in a subprocess (`compiler_bisector run`), so the bisector results only contained an unhelpful numerical index for the broken function.
    
    Additionally, fixes an ordering bug in the debug info saving logic.
    
    ghstack-source-id: 224fdf9cbfb4c35bcb0ec914b772ecea36ab87c0
    Pull-Request: https://github.com/pytorch/pytorch/pull/181452

--- torch/_inductor/graph.py
+++ torch/_inductor/graph.py
@@ -1837,7 +1837,7 @@ class GraphLowering(torch.fx.Interpreter):
             V.set_current_node(n),
         ):
             if (
-                n.op == "call_function"
+                is_call_function
                 # this path only for built-in operators
                 and n.target
                 and isinstance(n.target, torch._ops.OpOverload)
